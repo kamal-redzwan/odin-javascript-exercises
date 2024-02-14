1e6af767645343e71b277eedf6df66a61d723904
Kamal Redzwan <109417261+kamal-redzwan@users.noreply.github.com>
2024-02-14 15:25:44 +0800

    Added Solution for 01 - 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, multiple) {
+  let repeatedString = '';
 
+  if (multiple < 0) return 'ERROR';
+
+  for (let i = 0; i < multiple; i++) {
+    repeatedString += string;
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
