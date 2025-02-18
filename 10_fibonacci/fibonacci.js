a97aadab4403013aa2803582a2abe98058a7fbbc
Kamal Redzwan <109417261+kamal-redzwan@users.noreply.github.com>
2025-02-18 15:12:21 +0800

    done with exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (sequence) {
+  // check if seq is a number, if not change it
+  let count = typeof sequence === 'number' ? sequence : parseInt(sequence);
 
+  // return OOPS if negative num
+  if (count < 0) return 'OOPS';
+
+  // return 0 if the 1st sequence is 0
+  if (count === 0) return 0;
+
+  // create an array with the length of count
+  return Array.from({ length: count }).reduce(
+    // each iteration update the sequence to this:-
+    // put secondSec to firstSec
+    // add firstSec with secondSec
+    ([firstSeq, secondSec]) => [secondSec, firstSeq + secondSec],
+
+    // set initial value to [0, 1] <- the first 2 fib sequence
+    [0, 1]
+
+    // return the 1st value of the array [0]
+  )[0];
 };
 
 // Do not edit below this line
