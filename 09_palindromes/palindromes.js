16743ac1146c162e2bbb3e233bc8ddd8ddf43804
Kamal Redzwan <109417261+kamal-redzwan@users.noreply.github.com>
2025-02-18 13:55:59 +0800

    done with exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // [^a-z0-9]/g = remove all non-alphanumeric character
+  const processRegex = string.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+  // reverse the string and compare
+  return processRegex.split('').reverse().join('') == processRegex;
 };
 
 // Do not edit below this line
